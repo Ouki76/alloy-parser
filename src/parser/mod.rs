54b856a754dfb26cf87c238f1fd6753d443815ed
Ouki76 <ouki76@proton.me>
2026-05-07 18:04:20 +0600

    Item into url

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -7,12 +7,32 @@ use tokio::io::{AsyncBufReadExt, BufReader};
 use url::Url;
 
 pub struct Item {
+    pub scheme: String,
     pub uuid: String,
     pub host: Option<String>,
     pub port: Option<u16>,
     pub params: Vec<(Box<str>, Box<str>)>,
 }
 
+impl From<Item> for Url {
+    fn from(value: Item) -> Url {
+        let host = value.host.unwrap_or_default();
+
+        let mut url = Url::parse(&format!("{}://{}@{}", value.scheme, value.uuid, host)).unwrap();
+
+        url.set_port(value.port).unwrap();
+
+        {
+            let mut pairs = url.query_pairs_mut();
+
+            value.params.iter().for_each(|(k, v)| {
+                pairs.append_pair(k, v);
+            });
+        }
+
+        url
+    }
+}
 pub async fn parse<L>(loader: L) -> Result<Vec<Item>, Error>
 where
     L: Loader,
@@ -28,7 +48,8 @@ where
         if let Ok(url) = Url::parse(&line) {
             // Create item
             let item = Item {
-                uuid: url.username().to_string(),
+                scheme: url.scheme().to_owned(),
+                uuid: url.username().to_owned(),
                 host: url.host().map(|h| h.to_string()),
                 port: url.port(),
                 params: url
@@ -171,5 +192,40 @@ vless://22222222-d105-4ee4-b904-1f6ed8417f4d@google.com:8443?path=/ws&host=cdn.g
         let (_server, loader) = create_remote_loader();
 
         assert_items(loader).await;
+    }
+
+    #[test]
+    fn item_into_url() {
+        let item = Item {
+            scheme: "vless".to_string(),
+            uuid: "7d3e6808-d105-4ee4-b904-1f6ed8417f4d".to_string(),
+            host: Some("8.218.32.188".to_string()),
+            port: Some(3004),
+            params: vec![
+                ("type".into(), "ws".into()),
+                ("host".into(), "speedtest.net".into()),
+            ],
+        };
+
+        let url: Url = item.into();
+
+        assert_eq!(url.scheme(), "vless");
+        assert_eq!(url.username(), "7d3e6808-d105-4ee4-b904-1f6ed8417f4d");
+        assert_eq!(url.host_str(), Some("8.218.32.188"));
+        assert_eq!(url.port(), Some(3004));
+
+        assert_eq!(
+            url.query_pairs()
+                .find(|(k, _)| k == "type")
+                .map(|(_, v)| v.into_owned()),
+            Some("ws".to_string())
+        );
+
+        assert_eq!(
+            url.query_pairs()
+                .find(|(k, _)| k == "host")
+                .map(|(_, v)| v.into_owned()),
+            Some("speedtest.net".to_string())
+        );
     }
 }
